b03adda8172c88ca2ba7c9e51d61c8d2f95e9170
TylerHardwick <115378400+TylerHardwick@users.noreply.github.com>
2022-10-09 00:45:48 +0100

    Delete main(1).py

--- main(1).py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the the Band Name Creator!\n")
-city = input("What city did you grow up in? \n")
-pet = input("What is the name of your pet? \n")
-print("Your Band name is... " + city + " " + pet +"!" )
