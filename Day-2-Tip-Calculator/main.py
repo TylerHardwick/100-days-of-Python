b0725751eed89ebfcec69cc8202647f9b06a5ecd
TylerHardwick <115378400+TylerHardwick@users.noreply.github.com>
2022-10-09 00:40:25 +0100

    Update main.py

--- Day-2-Tip-Calculator/main.py
+++ Day-2-Tip-Calculator/main.py
@@ -3,5 +3,5 @@ bill = float(input("What was the total bill?\n £"))
 tip = float("1."+(input("What percentage of tip would you like to give?\n")))
 split = int(input("How many people are splitting the bill?\n"))
 total = round((bill / split) * tip,2) 
-
-print(f"Each person should pay £{total}")
+rounded_total = "{:.2f}".format(total)
+print(f"Each person should pay £{rounded_total}")
