577c87d6d24debb7876e545f84b3f238237295c0
TylerHardwick <115378400+TylerHardwick@users.noreply.github.com>
2022-10-09 00:45:11 +0100

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,4 @@
+print("Welcome to the the Band Name Creator!\n")
+city = input("What city did you grow up in? \n")
+pet = input("What is the name of your pet? \n")
+print("Your Band name is... " + city + " " + pet +"!" )
