6788bf2fce0048eeb309a25594f324c14710c1fa
TylerHardwick <115378400+TylerHardwick@users.noreply.github.com>
2022-10-16 13:00:43 +0100

    Delete Day-2-Tip-Calculator directory

--- Day-2-Tip-Calculator/main.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator! o/\n")
-bill = float(input("What was the total bill?\n £"))
-tip = float("1."+(input("What percentage of tip would you like to give?\n")))
-split = int(input("How many people are splitting the bill?\n"))
-total = round((bill / split) * tip,2) 
-rounded_total = "{:.2f}".format(total)
-print(f"Each person should pay £{rounded_total}")
