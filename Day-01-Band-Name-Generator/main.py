161b1ffc17381cfc88600ee5577dd97f6a7a54d5
Tyler Hardwick <115378400+TylerHardwick@users.noreply.github.com>
2024-12-02 12:41:35 +0000

    Update main.py

--- Day-01-Band-Name-Generator/main.py
+++ Day-01-Band-Name-Generator/main.py
@@ -1,4 +1,4 @@
 print("Welcome to Band Name Generator")
 colour = input("\nWhat colour is the shirt you are wearing?: ").capitalize()
 pet = input("What was the type of animal was your first pet?: ").capitalize()
-print(f"\n\nYour band name is: ... the {colour} {pet}s")
+print(f"\n\nYour band name is: ... The {colour} {pet}s")
