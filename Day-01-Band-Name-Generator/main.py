7e19ed5eff325e947efd3717d73f45f85453b71f
Tyler Hardwick <115378400+TylerHardwick@users.noreply.github.com>
2024-12-02 12:35:53 +0000

    Update main.py

--- Day-01-Band-Name-Generator/main.py
+++ Day-01-Band-Name-Generator/main.py
@@ -1,7 +1,4 @@
-print("Welcome to the tip calculator! o/\n")
-bill = float(input("What was the total bill?\n £"))
-tip = float("1."+(input("What percentage of tip would you like to give?\n")))
-split = int(input("How many people are splitting the bill?\n"))
-total = round((bill / split) * tip,2) 
-
-print(f"Each person should pay £ {total}")
+print("Welcome to Band Name Generator")
+colour = input("\nWhat colour is the shirt you are wearing?: ").capitalize()
+pet = input("What was the name of your first pet?: ").capitalize()
+print(f"\n\nYour band name is: ...{colour} {pet}")
